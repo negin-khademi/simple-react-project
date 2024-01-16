daab38e0cb60c18a74bb6c11576d467305fd95c0
Ngin <negin.khademi@gmail.com>
2024-01-16 17:16:23 +0330

    use props

---
+++ src/BlogList.js
@@ -0,0 +1,13 @@
+const BlogList = (props) => {
+    return ( <div className="blog-list">
+        {blogs.map((blog)=>(
+            <div className="blog-preview" key={blog.id}>
+                <h2>{blog.title}</h2>
+                <p>wrriten by {blog.author}</p>
+            </div>
+        ))}
+         
+    </div> );
+}
+ 
+export default BlogList;
--- src/Home.js
+++ src/Home.js
@@ -1,8 +1,26 @@
+import { useState } from "react";
+import BlogList from "./BlogList";
+
 const Home = () => {
+  const [blogs,setBlogs] = useState([
+    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
+    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
+    { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
+  ])
+    // const [name, setName] = useState('mario');
+    // const [age, setAge] = useState(25);
+    // const handleClick = ()=>{
+    //     setName('negin')
+    //     setAge(30);
+    // }
+
     return (
       <div className="home">
-        <h2>Homepage</h2>
-      </div>
+        <BlogList blogs={blogs} key={blogs.id}/>
+        {/* <h2>Homepage</h2> */}
+        {/* <p>{name} is {age} years old</p>
+        <button onClick={handleClick}>click me</button>  */}
+    </div>
     );
   }
    
